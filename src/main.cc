6958f0699ae6b832cd5448f877f778df150dc425
Mustafa Siddiqui <mustafa.eins@gmail.com>
2022-06-24 18:18:13 -0400

    add current main file

--- src/main.cc
+++ src/main.cc
@@ -7,21 +7,21 @@
  *
  */
 
-#include "../templates/node.h"
+#include "templates/node.h"
 #include <iostream>
 
 int main(void)
 {
-    NODE<int> IntNode(12, "one");
+    NODE<int> IntNode("one", 12);
     std::cout << IntNode << std::endl;
 
-    NODE<double> DoubleNode(12.12, "two");
+    NODE<double> DoubleNode("two", 12.22);
     std::cout << DoubleNode << std::endl;
 
-    NODE<char> CharNode('c', "three");
+    NODE<char> CharNode("three", 'c');
     std::cout << CharNode << std::endl;
 
-    NODE<std::string> StringNode("hello", "four");
+    NODE<std::string> StringNode("four", "hello");
     std::cout << StringNode << std::endl;
 
     return 0;
