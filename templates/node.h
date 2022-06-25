660687e5e61b0f7982448f4620cac5b685c251d1
Mustafa Siddiqui <mustafa.eins@gmail.com>
2022-06-25 00:33:26 -0400

    add next and prev note ptrs and relevant methods

--- templates/node.h
+++ templates/node.h
@@ -17,7 +17,6 @@
  */
 #include "interfaces/node-intf.h"
 #include <iostream>
-#include <string>
 
 /**
  * =====================================================
@@ -28,52 +27,79 @@
 /**
  * @brief Template class for a node. Inherits from the
  * interface for a node class.
- * @tparam T typename
  */
 template <typename T>
 class NODE : public NODE_INTF<T> {
 private:
-    std::string myId;
+    const std::string myId;
     T myVal;
+    std::shared_ptr<NODE_INTF<T>> nextNodePtr;
+    std::shared_ptr<NODE_INTF<T>> prevNodePtr;
 
 public:
-    // delete default & copy constructors and assignment operator.
+    // delete default constructor
     NODE() = delete;
-    NODE(NODE const&) = delete;
-    NODE& operator=(NODE const&) = delete;
 
     // constructor
     explicit NODE(std::string id, T value)
         : NODE_INTF<T>()
         , myId(id)
         , myVal(value)
+        , nextNodePtr(nullptr)
+        , prevNodePtr(nullptr)
     {
     }
 
+    /**
+     * =====================================================
+     * INTERFACE METHODS DEFINITIONS
+     * =====================================================
+     */
+
     // destructor
-    ~NODE() override {};
+    ~NODE() override { }
 
     // return id of object
-    std::string getID(void) const override
+    std::string getID() const override
     {
         return this->myId;
     }
 
     // return value stored in object
-    T getValue(void) const override
+    T getValue() const override
     {
         return this->myVal;
     }
 
     // declaration and definition is split due to
-    // template specializations required.
-    std::string toString(void) const override;
+    // template specializations required
+    std::string toString() const override;
 
-    /**
-     * @brief Overload stream insertion operator.
-     * @param out Output stream
-     * @param node Object of Node<T> class.
-     */
+    // set next node to given node
+    void setNextNodePtr(std::shared_ptr<NODE_INTF<T>>& nodePtr) override
+    {
+        this->nextNodePtr = nodePtr;
+    }
+
+    // get pointer to the next node
+    std::shared_ptr<NODE_INTF<T>> getNextNodePtr() const override
+    {
+        return this->nextNodePtr;
+    }
+
+    // set previous node to given node
+    void setPrevNodePtr(std::shared_ptr<NODE_INTF<T>>& nodePtr) override
+    {
+        this->prevNodePtr = nodePtr;
+    }
+
+    // get pointer to the previous node
+    std::shared_ptr<NODE_INTF<T>> getPrevNodePtr() const override
+    {
+        return this->prevNodePtr;
+    }
+
+    // '<<' operator overload
     friend std::ostream& operator<<(std::ostream& os, const NODE<T>& nodeObj)
     {
         os << nodeObj.toString();
