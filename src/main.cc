bb6e5421920450146ad6cb154f5ddcd147a6c656
Mustafa Siddiqui <mustafa.eins@gmail.com>
2022-07-25 19:12:54 -0400

    call `appendLast()` and `insertFirst() methods

--- src/main.cc
+++ src/main.cc
@@ -19,6 +19,9 @@ int main(void) {
     NODE<int> IntNode2("one-two", 69);
     std::cout << IntNode2 << std::endl;
 
+    NODE<int> IntNode3("one-three", 70);
+    NODE<int> IntNode4("one-four", 99);
+
     NODE<double> DoubleNode("two", 12.22);
     std::cout << DoubleNode << std::endl;
 
@@ -31,6 +34,11 @@ int main(void) {
     LINKED_LIST<int> LinkedList(
         std::make_shared<NODE<int>>(IntNode),
         std::make_shared<NODE<int>>(IntNode2));
+    std::cout << std::endl << LinkedList << std::endl;
+
+    LinkedList.appendLast(std::make_shared<NODE<int>>(IntNode3));
+    LinkedList.insertFirst(std::make_shared<NODE<int>>(IntNode4));
+
     std::cout << std::endl << LinkedList << std::endl;
 
     return 0;
