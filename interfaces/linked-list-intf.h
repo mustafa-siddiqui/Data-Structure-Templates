302060005992fa8a9291e242459f2018806701bc
Mustafa Siddiqui <mustafa.eins@gmail.com>
2022-07-25 19:08:25 -0400

    update methods to have a shared pointer as parameter

--- interfaces/linked-list-intf.h
+++ interfaces/linked-list-intf.h
@@ -16,6 +16,7 @@
  * =====================================================
  */
 
+#include <memory>
 #include <string>
 
 #include "expected-lite/include/nonstd/expected.hpp"
@@ -48,54 +49,60 @@ public:
 
     /**
      * @brief Append a node to the end of the list.
-     * @param node A node object
+     * @param nodePtr A node object pointer
      */
-    virtual void appendLast(NODE_INTF<T> node) = 0;
+    virtual void appendLast(
+        std::shared_ptr<NODE_INTF<T>> nodePtr) = 0;
 
     /**
      * @brief Insert a node to the head of the list. Will
      * replace the head node of the list and will point to it.
-     * @param node A node object
+     * @param nodePtr A node object pointer
      */
-    virtual void insertFirst(NODE_INTF<T> node) = 0;
+    virtual void insertFirst(
+        std::shared_ptr<NODE_INTF<T>> nodePtr) = 0;
 
     /**
      * @brief Insert a node after a specified node in the list
      * (if it exists in the list). If specified does not exist,
      * do nothing.
-     * @param nodePresent Specified node in the list.
-     * @param nodeToBeInserted Node object to be inserted in the list.
+     * @param nodePresentPtr Specified node pointer in the list.
+     * @param nodeToBeInsertedPtr Node object pointer to be inserted
+     * in the list.
      */
     virtual nonstd::expected<void, ERROR_CODES> insertBefore(
-        NODE_INTF<T>& nodePresent, NODE_INTF<T> nodeToBeInserted) = 0;
+        std::shared_ptr<NODE_INTF<T>> nodePresentPtr,
+        std::shared_ptr<NODE_INTF<T>> nodeToBeInsertedPtr) = 0;
 
     /**
      * @brief Insert a node before a specified node in the list (if
      * it exists in the list). If specified node does not exist,
      * do nothing.
-     * @param nodePresent Specified node in the list.
-     * @param nodeToBeInserted Node object to be inserted in the list.
+     * @param nodePresentPtr Specified node pointer in the list.
+     * @param nodeToBeInsertedPtr Node object pointer to be inserted
+     * in the list.
      * @return nothing if successful, error code otherwise
      */
     virtual nonstd::expected<void, ERROR_CODES> insertAfter(
-        NODE_INTF<T>& nodePresent, NODE_INTF<T> nodeToBeInserted) = 0;
+        std::shared_ptr<NODE_INTF<T>> nodePresentPtr,
+        std::shared_ptr<NODE_INTF<T>> nodeToBeInsertedPtr) = 0;
 
     /**
      * @brief Delete given node from the list. Do nothing if node
      * not present in the list.
-     * @param nodeToBeDeleted Node to delete from list
+     * @param nodeToBeDeletedPtr Pointer of node to delete from list
      * @return nothing if successful, error code otherwise
      */
     virtual nonstd::expected<void, ERROR_CODES> deleteNode(
-        NODE_INTF<T> nodeToBeDeleted) = 0;
+        std::shared_ptr<NODE_INTF<T>> nodeToBeDeletedPtr) = 0;
 
     /**
      * @brief Search for a node in the list.
-     * @param node Node to search for in the list
+     * @param nodePtr Pointer node to search for in the list
      * @return ID of string if present, error otherwise
      */
     virtual nonstd::expected<std::string, ERROR_CODES> find(
-        NODE_INTF<T> node) const = 0;
+        std::shared_ptr<NODE_INTF<T>> nodePtr) const = 0;
 
     /**
      * @brief Convert linked list into a string representation.
