819395fe22ffaf627763d3498dfb06356db374b7
Mustafa Siddiqui <mustafa.eins@gmail.com>
2022-07-26 23:44:48 -0400

    add test code

--- src/main.cc
+++ src/main.cc
@@ -13,35 +13,40 @@
 #include "templates/node.h"
 
 int main(void) {
-    NODE<int> IntNode(0, 12);
+    NODE<int> IntNode(99);
     std::cout << IntNode << std::endl;
 
-    NODE<int> IntNode2(1, 69);
+    NODE<int> IntNode2(69);
     std::cout << IntNode2 << std::endl;
 
-    NODE<int> IntNode3(2, 70);
-    NODE<int> IntNode4(3, 99);
-
-#ifdef DEBUG
-    NODE<double> DoubleNode(4, 12.22);
-    std::cout << DoubleNode << std::endl;
-
-    NODE<char> CharNode(5, 'c');
-    std::cout << CharNode << std::endl;
-
-    NODE<std::string> StringNode(6, "hello");
-    std::cout << StringNode << std::endl;
-#endif
+    NODE<int> IntNode3(70);
+    NODE<int> IntNode4(1000);
 
     LINKED_LIST<int> LinkedList(
         std::make_shared<NODE<int>>(IntNode),
         std::make_shared<NODE<int>>(IntNode2));
     std::cout << std::endl << LinkedList << std::endl;
+    std::cout << "Size: " << LinkedList.getSize() << std::endl;
 
     LinkedList.appendLast(std::make_shared<NODE<int>>(IntNode3));
     LinkedList.insertFirst(std::make_shared<NODE<int>>(IntNode4));
 
     LinkedList.print();
+    std::cout << "Size: " << LinkedList.getSize() << std::endl;
+
+    // test
+    bool res = true;
+    if (!(*LinkedList.getHead() == IntNode4)) {
+        res = false;
+        std::cout << LinkedList.getHead()->toString() << ", "
+                  << IntNode4.toString() << std::endl;
+    }
+    std::cout << "\nres: " << res << std::endl;
+
+    std::cout << "\nDeleting node\n";
+    LinkedList.deleteNode(std::make_shared<NODE<int>>(IntNode4));
+    LinkedList.print();
+    std::cout << "Size: " << LinkedList.getSize() << std::endl;
 
     return 0;
 }
